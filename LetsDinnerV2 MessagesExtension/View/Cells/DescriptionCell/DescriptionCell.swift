5925b56b86d3b85b2857d898f02550700ec36b35
Alex Cheung <alexxcheungg@gmail.com>
2019-11-20 20:18:06 +0800

    Description Cell UI updated

--- LetsDinnerV2 MessagesExtension/View/Cells/DescriptionCell/DescriptionCell.swift
+++ LetsDinnerV2 MessagesExtension/View/Cells/DescriptionCell/DescriptionCell.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class DescriptionCell: UITableViewCell {
+class DescriptionCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource {
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var recipesCollectionView: UICollectionView!
+    
+    private let selectedRecipes = Event.shared.selectedRecipes
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.recipesCollectionView.dataSource = self
+        self.recipesCollectionView.delegate = self
+        self.recipesCollectionView.register(UINib(nibName: CellNibs.recipeCVCell, bundle: nil), forCellWithReuseIdentifier: CellNibs.recipeCVCell)
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 100)
+        self.recipesCollectionView.collectionViewLayout = layout
+        
+        self.descriptionLabel.backgroundColor = nil
+        self.descriptionLabel.textColor = Colors.dullGray
     }
     
     override func layoutSubviews() {
@@ -27,5 +43,16 @@ class DescriptionCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return selectedRecipes.count
+     }
+     
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         let recipeCVCell = collectionView.dequeueReusableCell(withReuseIdentifier: CellNibs.recipeCVCell, for: indexPath) as! RecipeCVCell
+         let recipe = selectedRecipes[indexPath.row]
+         recipeCVCell.configureCell(recipeTitle: recipe.title ?? "")
+         return recipeCVCell
+     }
     
 }
