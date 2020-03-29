7d765d9683c82fd9a5ee006cb8af8df2ccf70b38
Eric Ordonneau <eric.ordonneau@outlook.fr>
2020-03-29 16:28:04 +0200

    Worked on CloudManager

--- LetsDinnerV2 MessagesExtension/Manager/CloudManager.swift
+++ LetsDinnerV2 MessagesExtension/Manager/CloudManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CloudKit
+import RealmSwift
 
 class CloudManager {
     
@@ -155,9 +156,339 @@ class CloudManager {
         
     }
     
-    func fetchIngredientsForCustomRecipeOnCloud() {
+        private func fetchCustomRecipesOnCloud(completion: @escaping (Result<[CKRecord], Error>) -> Void) {
+            let predicate = NSPredicate(value: true)
+            let sort = NSSortDescriptor(key: "creationDate", ascending: false)
+            let query = CKQuery(recordType: "CustomRecipe", predicate: predicate)
+            query.sortDescriptors = [sort]
+            
+    //        Check how to use operation, seems to be used to fetch all in one, better.
+    //        let operation = CKQueryOperation(query: query)
+            
+    //        var customRecipes = [CustomRecipe]()
+    //        var customRecipesModel = [CustomRecipeModel]()
+            
+            privateDatabase.perform(query, inZoneWith: nil) { [weak self] (records, error) in
+                guard let self = self else { return }
+                if let error = error as? CKError {
+                    self.dealWithCloudKitError(error)
+                } else if error != nil {
+                    DispatchQueue.main.async {
+                         completion(.failure(error!))
+                    }
+                }
+                guard let records = records else { return }
+                completion(.success(records))
+    //            records.forEach { record in
+    ////                customRecipes.append(self.convertCloudRecipeToRealmRecipe(record))
+    //                customRecipesModel.append(self.convertCloudRecipeToRecipeModel(record))
+    //            }
+    //            completion(.success(customRecipesModel))
+
+            }
+            
+    //        operation.recordFetchedBlock = { record in
+    //            let customRecipe = CustomRecipe()
+    //
+    //        }
+            
+        }
         
-    }
+        private func fetchCorrespondingCustomIngredientsOnCloud(for recordID: CKRecord.ID?, completion: @escaping (Result<[CustomIngredientModel], Error>) -> Void) {
+            guard let recordID = recordID else { return }
+            let predicate = NSPredicate(format: "parentRecipe = %@", recordID)
+            let sort = NSSortDescriptor(key: "creationDate", ascending: false)
+            let query = CKQuery(recordType: "CustomIngredient", predicate: predicate)
+            query.sortDescriptors = [sort]
+            
+            var customIngredientsModel = [CustomIngredientModel]()
+            
+            privateDatabase.perform(query, inZoneWith: nil) { [weak self] (records, error) in
+                guard let self = self else { return }
+                if let error = error as? CKError {
+                    self.dealWithCloudKitError(error)
+                } else if error != nil {
+                    DispatchQueue.main.async {
+                        completion(.failure(error!))
+                    }
+                }
+                guard let records = records else { return }
+                customIngredientsModel = self.convertCloudIngredientsToIngredientsModel(records)
+                completion(.success(customIngredientsModel))
+            }
+        }
+        
+        struct CustomRecipeModel {
+            var title: String = ""
+            var id: String = ""
+            var servings: Int = 0
+            var downloadUrl: String? = nil
+            var cookingSteps: [String]? = nil
+            var comments: String? = nil
+            var ingredients: [CustomIngredientModel]? = nil
+            var recordID: CKRecord.ID? = nil
+        }
+        
+        struct CustomIngredientModel {
+            var name: String = ""
+            var amount: Double? = nil
+            var unit: String? = nil
+            var parentRecipe: CKRecord.Reference? = nil
+        }
+        
+
+        
+    //    func fetchIngredientsForCustomRecipeOnCloud() {
+    //
+    //    }
+        
+        func convertCloudRecipeToRecipeModel(_ cloudRecipe: CKRecord) -> CustomRecipeModel {
+            guard cloudRecipe.recordType == "CustomRecipe" else { return CustomRecipeModel() }
+            var customRecipe = CustomRecipeModel()
+            if let title = cloudRecipe.value(forKey: CustomRecipeKey.title.rawValue) as? String {
+                customRecipe.title = title
+            }
+            if let id = cloudRecipe.value(forKey: CustomRecipeKey.id.rawValue) as? String {
+                customRecipe.id = id
+            }
+            if let servings = cloudRecipe.value(forKey: CustomRecipeKey.servings.rawValue) as? Int {
+                customRecipe.servings = servings
+            }
+            if let downloadUrl = cloudRecipe.value(forKey: CustomRecipeKey.downloadUrl.rawValue) as? String {
+                customRecipe.downloadUrl = downloadUrl
+            }
+            if let comments = cloudRecipe.value(forKey: CustomRecipeKey.comments.rawValue) as? String {
+                customRecipe.comments = comments
+            }
+            if let cloudCookingSteps = cloudRecipe.value(forKey: CustomRecipeKey.cookingSteps.rawValue) as? [String] {
+                customRecipe.cookingSteps = cloudCookingSteps
+            }
+            customRecipe.recordID = cloudRecipe.recordID
+     
+            return customRecipe
+        }
+        
+        func convertCloudIngredientsToIngredientsModel(_ cloudIngredients: [CKRecord]) -> [CustomIngredientModel] {
+            var customIngredients = [CustomIngredientModel]()
+            cloudIngredients.forEach { cloudIngredient in
+                var customIngredient = CustomIngredientModel()
+                if let name = cloudIngredient.value(forKey: CustomIngredientKey.name.rawValue) as? String {
+                    customIngredient.name = name
+                }
+                if let amount = cloudIngredient.value(forKey: CustomIngredientKey.amount.rawValue) as? Double {
+                    customIngredient.amount = amount
+                }
+                if let unit = cloudIngredient.value(forKey: CustomIngredientKey.unit.rawValue) as? String {
+                    customIngredient.unit = unit
+                }
+                if let reference = cloudIngredient.value(forKey: CustomIngredientKey.parentRecipe.rawValue) as? CKRecord.Reference {
+                    customIngredient.parentRecipe = reference
+                }
+                customIngredients.append(customIngredient)
+            }
+            return customIngredients
+        }
+        
+        func convertRecipeModelToRealmRecipe(_ modelRecipe: CustomRecipeModel) -> CustomRecipe {
+            
+            return CustomRecipe()
+        }
+        
+        func fetchRecipesAndIngredientsFromCloud(completion: @escaping (Result<[CustomRecipe], Error>) -> Void) {
+    //        fetchRecipesWithoutIngredients
+            self.fetchCustomRecipesOnCloud { result in
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        completion(.failure(error))
+                    }
+                case .success(let cloudRecipes):
+                    //        convertRecipesToModelObject
+                    var customRecipesModel = [CustomRecipeModel]()
+                    cloudRecipes.forEach { cloudRecipe in
+                        customRecipesModel.append(self.convertCloudRecipeToRecipeModel(cloudRecipe))
+                    }
+                    //        fetchCorrespondingIngredients
+                    //        convertIngredientsToModelObjects
+                    var updatedCustomRecipesModel = [CustomRecipeModel]()
+                    customRecipesModel.forEach { customRecipeModel in
+                        self.fetchCorrespondingCustomIngredientsOnCloud(for: customRecipeModel.recordID) { result in
+                            switch result {
+                            case .failure(let error):
+                                DispatchQueue.main.async {
+                                    completion(.failure(error))
+                                }
+                            case .success(let customIngredientsModel):
+                                var updatedCustomRecipeModel = customRecipeModel
+                                updatedCustomRecipeModel.ingredients = customIngredientsModel
+                                updatedCustomRecipesModel.append(updatedCustomRecipeModel)
+                               
+                                
+                            }
+                        }
+                    }
+                     //        convertModelObjectsToRealmRecipes
+                    DispatchQueue.main.async {
+                        completion(.success(self.convertModelRecipesToRealmRecipes(updatedCustomRecipesModel)))
+                    }
+                    
+                    
+                    
+                    
+                }
+            }
+
+
+
+        }
+        
+        func convertCloudRecipeToRealmRecipe(_ cloudRecipe: CKRecord) -> CustomRecipe {
+            guard cloudRecipe.recordType == "CustomRecipe" else { return CustomRecipe() }
+            let customRecipe = CustomRecipe()
+            if let title = cloudRecipe.value(forKey: CustomRecipeKey.title.rawValue) as? String {
+                customRecipe.title = title
+            }
+            if let id = cloudRecipe.value(forKey: CustomRecipeKey.id.rawValue) as? String {
+                customRecipe.id = id
+            }
+            if let servings = cloudRecipe.value(forKey: CustomRecipeKey.servings.rawValue) as? Int {
+                customRecipe.servings = servings
+            }
+            if let downloadUrl = cloudRecipe.value(forKey: CustomRecipeKey.downloadUrl.rawValue) as? String {
+                customRecipe.downloadUrl = downloadUrl
+            }
+            if let comments = cloudRecipe.value(forKey: CustomRecipeKey.comments.rawValue) as? String {
+                customRecipe.comments = comments
+            }
+            var cookingSteps = [String]()
+            if let cloudCookingSteps = cloudRecipe.value(forKey: CustomRecipeKey.cookingSteps.rawValue) as? [String] {
+                cookingSteps = cloudCookingSteps
+            }
+            cookingSteps.forEach { step in
+                customRecipe.cookingSteps.append(step)
+            }
+            return CustomRecipe()
+        }
+        
+        func convertModelRecipesToRealmRecipes(_ modelRecipes: [CustomRecipeModel]) -> [CustomRecipe] {
+            var customRecipes = [CustomRecipe]()
+            modelRecipes.forEach { modelRecipe in
+                let customRecipe = CustomRecipe()
+                customRecipe.title = modelRecipe.title
+                customRecipe.id = modelRecipe.id
+                customRecipe.downloadUrl = modelRecipe.downloadUrl
+                customRecipe.servings = modelRecipe.servings
+                customRecipe.comments = modelRecipe.comments
+                let cookingSteps = List<String>()
+                customRecipe.cookingSteps.forEach { step in
+                    cookingSteps.append(step)
+                }
+                customRecipe.cookingSteps = cookingSteps
+                if let modelIngredients = modelRecipe.ingredients {
+                    customRecipe.ingredients = self.convertModelIngredientsToRealmIngredients(modelIngredients)
+                }
+                customRecipes.append(customRecipe)
+            }
+            return customRecipes
+        }
+        
+        func convertModelIngredientsToRealmIngredients(_ modelIngredients: [CustomIngredientModel]) -> List<CustomIngredient>{
+            let customIngredients = List<CustomIngredient>()
+            modelIngredients.forEach { modelIngredient in
+                let customIngredient = CustomIngredient()
+                customIngredient.name = modelIngredient.name
+                customIngredient.amount.value = modelIngredient.amount
+                customIngredient.unit = modelIngredient.unit
+                customIngredients.append(customIngredient)
+            }
+            return customIngredients
+        }
+        
+        func convertCloudIngredientToRealmIngredient(_ cloudIngredient: CKRecord) -> CustomIngredient {
+            
+            return CustomIngredient()
+        }
+        
+        func dealWithError(_ error: Error) {
+            
+        }
+        
+        func dealWithCloudKitError(_ error: CKError) {
+            switch error.code {
+            case .alreadyShared:
+                print("already shared")
+            case .internalError:
+                print("already shared")
+            case .partialFailure:
+                print("already shared")
+            case .networkUnavailable:
+                print("already shared")
+            case .networkFailure:
+                print("already shared")
+            case .badContainer:
+                print("already shared")
+            case .serviceUnavailable:
+                print("already shared")
+            case .requestRateLimited:
+                print("already shared")
+            case .missingEntitlement:
+                print("already shared")
+            case .notAuthenticated:
+                print("already shared")
+            case .permissionFailure:
+                print("already shared")
+            case .unknownItem:
+                print("already shared")
+            case .invalidArguments:
+                print("already shared")
+            case .resultsTruncated:
+                print("already shared")
+            case .serverRecordChanged:
+                print("already shared")
+            case .serverRejectedRequest:
+                print("already shared")
+            case .assetFileNotFound:
+                print("already shared")
+            case .assetFileModified:
+                print("already shared")
+            case .incompatibleVersion:
+                print("already shared")
+            case .constraintViolation:
+                print("already shared")
+            case .operationCancelled:
+                print("already shared")
+            case .changeTokenExpired:
+                print("already shared")
+            case .batchRequestFailed:
+                print("already shared")
+            case .zoneBusy:
+                print("already shared")
+            case .badDatabase:
+                print("already shared")
+            case .quotaExceeded:
+                print("already shared")
+            case .zoneNotFound:
+                print("already shared")
+            case .limitExceeded:
+                print("already shared")
+            case .userDeletedZone:
+                print("already shared")
+            case .tooManyParticipants:
+                print("already shared")
+            case .referenceViolation:
+                print("already shared")
+            case .managedAccountRestricted:
+                print("already shared")
+            case .participantMayNeedVerification:
+                print("already shared")
+            case .serverResponseLost:
+                print("already shared")
+            case .assetNotAvailable:
+                print("already shared")
+            @unknown default:
+                  print("already shared")
+            }
+        }
     
     
 
